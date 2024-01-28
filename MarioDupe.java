8a236ff4e8ef2bf1177d8d20affc8b1b6a516815
edmun <edmun@DESKTOP-7U29T6P>
2024-01-28 12:55:28 -0800

    asadf

---
+++ MarioDupe.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class MarioDupe {
+	JFrame frame;
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 500;
+	MarioDupe(){
+		frame = new JFrame();
+	}
+	void setup() {
+		frame.size(WIDTH,HEIGHT);
+		
+		
+	}
+}
